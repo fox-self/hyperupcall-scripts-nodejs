078bfc9ce43ba4c2e546b6fbf6008703f5b9402d
Edwin Kofler <edwin@kofler.dev>
2025-05-10 23:47:21 -0700

    Fixes and updates

---
+++ config-prettier.cjs
@@ -0,0 +1,4 @@
+const config = require('@hyperupcall/prettier-config')
+
+// NOTE: Prettier config w/ ECMAScript modules does not work.
+module.exports = config
---
+++ config-prettier.d.ts
@@ -0,0 +1,3 @@
+declare const exports: Record<string, unknown>
+
+export = exports
